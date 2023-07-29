ad4a9f7fe6e5ad5fd283278e8d485d12df0ab015
w1nsid <61118207+w1nsid@users.noreply.github.com>
2023-07-29 14:01:20 +0100

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * bubble_sort - Orders an integer array in ascending sequence
+ *
+ * @num_array: The integer array to be sorted
+ * @arr_size: The size of the array
+ *
+ * Return: None
+ */
+void bubble_sort(int *num_array, size_t arr_size)
+{
+	size_t index;
+	int swap_temp, is_sorted;
+
+	if (!num_array || arr_size == 0)
+		return;
+
+	do
+	{
+		is_sorted = 1;
+		for (index = 0; index < arr_size - 1; index++)
+		{
+			if (num_array[index] > num_array[index + 1])
+			{
+				is_sorted = 0;
+				swap_temp = num_array[index];
+				num_array[index] = num_array[index + 1];
+				num_array[index + 1] = swap_temp;
+				print_array(num_array, arr_size);
+			}
+		}
+
+	} while (is_sorted == 0);
+}
