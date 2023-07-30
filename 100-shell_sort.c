72a433a740f8b2e1a7320056305b5ddcbc63565b
w1nsid <61118207+w1nsid@users.noreply.github.com>
2023-07-30 01:59:34 +0100

    .

---
+++ 100-shell_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Implements shell sort algorithm using insertion sort on
+ * sub-arrays.
+ * @a: The array to be sorted.
+ * @s: The size of the array.
+ */
+void shell_sort(int *a, size_t s)
+{
+	size_t g, o, i, t;
+	int v;
+
+	for (g = 1; g < s; g = 3 * g + 1)
+		;
+	for (g /= 3; g > 0; g /= 3)
+	{
+		for (o = 0; o < g; o++)
+		{
+			i = o;
+			while (i < s - g)
+			{
+				if (a[i] > a[i + g])
+				{
+					v = a[i];
+					a[i] = a[i + g];
+					a[i + g] = v;
+					for (t = i; t >= g; t -= g)
+					{
+						if (a[t] < a[t - g])
+						{
+							v = a[t];
+							a[t] = a[t - g];
+							a[t - g] = v;
+						} else
+							break;
+					}
+				} else
+					i += g;
+			}
+		}
+		print_array(a, s);
+	}
+}
